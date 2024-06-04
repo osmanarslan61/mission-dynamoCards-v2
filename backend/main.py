2a8ecc1ae474cb71ba45a1bda4cedbada580a5f9
Osman <osmanarslan61@gmail.com>
2024-06-04 15:50:34 -0400

    Organized Gen AI tools and functions

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, HttpUrl
 from fastapi.middleware.cors import CORSMiddleware
-
+from services.genai import YoutubeProcessor
 
 class VideoAnalysisRequest(BaseModel):
     youtube_link: HttpUrl
@@ -21,26 +21,11 @@ app.add_middleware(
 @app.post("/analyze-video")
 def analyze_video(request: VideoAnalysisRequest):
     #Doing the analysis
-    from langchain_community.document_loaders import YoutubeLoader
-    from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-    loader = YoutubeLoader.from_youtube_url(str(request.youtube_link), add_video_info=True)
-    docs = loader.load()
-    print(f"On load: {type(docs)}")
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    result = text_splitter.split_documents(docs)
-
-    print(f"{type(result)}")
-
-    author = result[0].metadata['author']
-    length = result[0].metadata['length']
-    title = result[0].metadata['title']
-    total_size = len(result)
+    processor = YoutubeProcessor()
+    result = processor.retrieve_youtube_documents(str(request.youtube_link))
 
     return {
-        "author":author,
-        "length": length,
-        "title": title,
-        "total_size": total_size
+        "result":result
     }
 
