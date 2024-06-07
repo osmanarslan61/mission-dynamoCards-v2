abe25bdf7cd433b8da1e41c218bca03c325dc987
Osman <osmanarslan61@gmail.com>
2024-06-07 12:45:37 -0400

    implemented a document summary method with langchain

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, HttpUrl
 from fastapi.middleware.cors import CORSMiddleware
-from services.genai import YoutubeProcessor
+
+from services.genai import (
+    YoutubeProcessor,
+    GeminiProcessor
+)
 
 class VideoAnalysisRequest(BaseModel):
     youtube_link: HttpUrl
@@ -25,7 +29,12 @@ def analyze_video(request: VideoAnalysisRequest):
     processor = YoutubeProcessor()
     result = processor.retrieve_youtube_documents(str(request.youtube_link))
 
+    genai_processor = GeminiProcessor(
+        model_name="gemini-pro",
+        project = "dynamoproject-424320"
+    )
+    summary =genai_processor.generate_document_summary(result, verbose=True)
     return {
-        "result":result
+        "summary": summary
     }
 
